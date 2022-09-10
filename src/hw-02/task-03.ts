3ebf942cc81eee3def1a8d3f97e195ccc2937a60
Oksana Stets <oksanastets73@gmail.com>
2022-09-10 23:21:50 +0300

    complete task-03

--- src/hw-02/task-03.ts
+++ src/hw-02/task-03.ts
@@ -1,9 +1,18 @@
-// Напиши фукцнию findLongestWord(string), которая принимает 
+// Напиши фукцнию findLongestWord(string), которая принимает
 // параметром произвольную строку(в строке будут только слова и пробелы)
 //  и возвращает самое длинное слово в этой строке.
 
-const findLongestWord = function(string: string) {
-  // твой код
+const findLongestWord = function (
+  string: string,
+) {
+  const array: string[] = string.split(' ');
+  let longest: string = array[0];
+  for (let i = 0; i < array.length - 1; i++) {
+    if (longest.length < array[i + 1].length) {
+      longest = array[i + 1];
+    }
+  }
+  return longest;
 };
 
 /*
@@ -14,4 +23,4 @@ console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); //
 console.log(findLongestWord('Google do a roll')); // 'Google'
 
 console.log(findLongestWord('May the force be with you')); // 'force'
-export { };
+export {};
