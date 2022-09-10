ebaf32513c3a8cfdeeea22e728a92ab4c96aef28
Oksana Stets <oksanastets73@gmail.com>
2022-09-10 16:05:26 +0300

    add tasks as blanks

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@
 // import "./hw-01/task-05.ts";
 // import "./hw-01/task-06.ts";
 
-import "./hw-02/task-01.ts";
-import "./hw-02/task-02.ts";
+// import "./hw-02/task-01.ts";
+// import "./hw-02/task-02.ts";
+import "./hw-02/task-03.ts";
 export {};
